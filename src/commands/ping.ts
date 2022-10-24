00c395e2a2418321680c2ecba9552c6b6f6a9f50
leflores <pflores.fisi22@gmail.com>
2022-10-24 01:06:08 -0500

    fix: set properly Interaction type

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,10 @@
-import { CommandInteraction } from 'discord.js';
+import { ChatInputCommandInteraction } from 'discord.js';
 import { ChatCommand } from 'src/types';
 
 const ping: ChatCommand = {
   name: 'ping',
   description: 'Replies with Pong!',
-  run: async (interaction: CommandInteraction) => {
+  run: async (interaction: ChatInputCommandInteraction) => {
     await interaction.reply(
       `Pong! ${Date.now() - interaction.createdTimestamp} ms :scream_cat:`,
     );
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,8 @@
-import { CommandInteraction } from 'discord.js';
+import { ChatInputCommandInteraction } from 'discord.js';
 
 interface ChatCommand {
   name: string;
   description: string;
-  run: (interaction: CommandInteraction) => Promise<void>;
+  options?: Array<unknown>;
+  run: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
