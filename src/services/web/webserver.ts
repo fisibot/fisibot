18d5d665ada743c50af38b51418628d6a690e542
leflores <pflores.fisi22@gmail.com>
2022-10-27 20:54:06 -0500

    fix(webserver): PORT logging

--- src/services/web/webserver.ts
+++ src/services/web/webserver.ts
@@ -1,12 +1,13 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 
-const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+const requestListener = (_req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200);
   res.end('Fisibot status: OK');
 };
 
+const PORT = process.env.PORT || 3000;
 const server = createServer(requestListener);
 
-server.listen(process.env.PORT ?? 3000, () => {
-  console.log('Server running at http://localhost:3000/');
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
