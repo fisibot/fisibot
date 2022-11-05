f5c54e72d50828f574b2edd5f7b2b8a745ef9395
leflores <pflores.fisi22@gmail.com>
2022-11-05 02:37:49 -0500

    feat: improve feedback on forms registration

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,5 @@
 import {
-  APIEmbedField, DiscordAPIError, Events, Message,
+  APIEmbedField, DiscordAPIError, Events, GuildMember, Message,
 } from 'discord.js';
 import { FisiClientEventObject } from '@fisitypes';
 import RegisteredMember from '@services/db/models/registeredMember';
@@ -23,9 +23,16 @@ const MessageCreateHandler: FisiClientEventObject<Events.MessageCreate> = {
         const registeredUser = parseFieldsToJSON(fields) as RegisteredMember;
         registeredUser.base = Number(registeredUser.base);
 
+        let newMember: GuildMember | undefined;
+
         try {
+          // Try to find the member in the guild
+          // See https://www.reddit.com/r/Discordjs/comments/slgr4v/how_do_cache_and_fetch_work_and_what_is_the/
+          newMember = await message.guild?.members.fetch(registeredUser.discordId);
+
           console.log('registering...', registeredUser);
           const { VERIFIED_ROLE_ID } = process.env;
+
           // Save user to db
           await collections.registrations?.insertOne(registeredUser);
 
@@ -33,24 +40,44 @@ const MessageCreateHandler: FisiClientEventObject<Events.MessageCreate> = {
           // Issues I encountered:
           // 1. The user is not in the guild
           // 2. Missing Permissions: https://stackoverflow.com/q/62360928
-          const newMember = await message.guild?.members.fetch(registeredUser.discordId);
           await newMember?.roles.add(VERIFIED_ROLE_ID!);
           message.react('👌');
+          newMember!.send(
+            'Bienvenido al Discord de la FISI!!!\n\n'
+            + 'Has desbloqueado todo el servidor, gracias por registrarte.\n'
+            + '`#ProyectoDiscordDeLaFISI`',
+          ).catch(() => {});
         }
         catch (error) {
-          if (error instanceof MongoServerError) {
-            message.reply(`Server error when registering: ${error.errmsg}`);
-            message.react('❌');
-          }
+          let errorMessage: string;
+
           if (error instanceof DiscordAPIError) {
-            message.reply(`API error when registering: ${error}`);
-            message.react('❌');
+            errorMessage = newMember
+              ? `API error when registering: ${error}`
+              : `Can't fetch user: ${error}`;
+          }
+          else if (error instanceof MongoServerError) {
+            // Mongo fails, but we have fetched the user
+            errorMessage = `Server error when registering: ${error.errmsg}`;
           }
           else {
-            message.reply(`Unknown error when registering: ${error}`);
+            errorMessage = `Unknown error when registering: ${error}`;
             console.error(error);
-            message.react('❌');
           }
+          try {
+            // Send error feedback to the user
+            newMember!.send(
+              'El equipo de FisiBot ha sufrido un problema (nuestro) al registrarte.\n\n'
+              + 'Estamos (_claramente_) solucionando el problema, pero mientras tanto, '
+              + 'puedes contactar a un administrador para que te registre manualmente.',
+            );
+            errorMessage += `. DM sent to \`${newMember?.user.tag}\``;
+          }
+          catch (_error) {
+            errorMessage += `. Could not send DM to \`${registeredUser.discordId}\``;
+          }
+          message.reply(errorMessage);
+          message.react('❌');
         }
       }
     }
