86be38b1a367113a312acd8293aac50e9d7e8619
leflores <pflores.fisi22@gmail.com>
2022-11-05 17:49:28 -0500

    style: format logging

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -13,20 +13,22 @@ commandPaths.forEach(async (commandPath) => {
   const isDir = !commandPath.includes('.');
 
   if (isDir) {
+    // Import commands with subcommands
     const commandName = commandPath;
     const commandModule = await import(`@commands/${commandName}/${commandName}`) as {
       default: FisiSlashCommandObject
     };
     botCommands[commandName] = commandModule.default;
-    console.log(` - ${commandName}`);
+    console.log(`   /${commandName} <subcommand>`);
   }
   else {
+    // Import commands without subcommands
     const commandName = commandPath.substring(0, commandPath.indexOf('.'));
     const commandModule = await import(`@commands/${commandName}`) as {
       default: FisiSlashCommandObject
     };
     botCommands[commandName] = commandModule.default;
-    console.log(` - ${commandName}`);
+    console.log(`   /${commandName}`);
   }
 });
 
