5b014d674da46794b96c6fd751049b79e9f71978
leflores <pflores.fisi22@gmail.com>
2022-11-05 17:42:17 -0500

    refactor: new commands structure

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,18 +4,30 @@ import { FisiSlashCommandObject } from '@fisitypes';
 const botCommands: Record<string, FisiSlashCommandObject> = {};
 
 const COMMANDS_PATH = __dirname;
-const commandNames = fs.readdirSync(COMMANDS_PATH);
+const commandPaths = fs.readdirSync(COMMANDS_PATH);
+console.log('🍈 Loading command handlers...');
 
 // Dynamically load all commands into `botCommands`
-commandNames.forEach(async (commandName) => {
-  if (commandName === 'index.ts') return; // skip self
+commandPaths.forEach(async (commandPath) => {
+  if (commandPath === 'index.ts') return; // skip self
+  const isDir = !commandPath.includes('.');
 
-  const commandFile = `${commandName}.ts`;
-  console.log(`Loading command: ${commandFile}`);
-  const commandModule = await import(`@commands/${commandName}/${commandFile}`) as {
-    default: FisiSlashCommandObject
-  };
-  botCommands[commandName] = commandModule.default;
+  if (isDir) {
+    const commandName = commandPath;
+    const commandModule = await import(`@commands/${commandName}/${commandName}`) as {
+      default: FisiSlashCommandObject
+    };
+    botCommands[commandName] = commandModule.default;
+    console.log(` - ${commandName}`);
+  }
+  else {
+    const commandName = commandPath.substring(0, commandPath.indexOf('.'));
+    const commandModule = await import(`@commands/${commandName}`) as {
+      default: FisiSlashCommandObject
+    };
+    botCommands[commandName] = commandModule.default;
+    console.log(` - ${commandName}`);
+  }
 });
 
 export default botCommands;
