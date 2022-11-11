ac6db71ae826345e664c810f3db66341188a0a8d
leflores <pflores.fisi22@gmail.com>
2022-11-11 00:11:17 -0500

    refactor: setup webhook event

---
+++ src/events/webhookMessageCreate.ts
@@ -0,0 +1,93 @@
+import {
+  APIEmbedField, DiscordAPIError, Events, GuildMember, Message,
+} from 'discord.js';
+import { FisiClientEventObject } from '@fisitypes';
+import RegisteredMember from '@services/db/models/registeredMember';
+import { collections } from '@services/db/mongo';
+import { MongoServerError } from 'mongodb';
+
+function parseFieldsToJSON(fields: APIEmbedField[]) {
+  const obj: { [key: string]: any } = {};
+  fields.forEach((field) => {
+    obj[field.name] = field.value;
+  });
+  return obj;
+}
+
+const MessageCreateHandler: FisiClientEventObject<Events.MessageCreate> = {
+  eventName: Events.MessageCreate,
+  handle: async (message: Message) => {
+    if (!message.webhookId) return;
+
+    const webhookMessage = message; // rename for clarity
+
+    if (message.content === '`#!fisibot/registrations`') {
+      const { fields } = webhookMessage.embeds[0];
+      const registeredUser = parseFieldsToJSON(fields) as RegisteredMember;
+      registeredUser.base = Number(registeredUser.base);
+
+      let newMember: GuildMember | undefined;
+
+      try {
+        // Try to find the member in the guild
+        // See https://www.reddit.com/r/Discordjs/comments/slgr4v/how_do_cache_and_fetch_work_and_what_is_the/
+        newMember = await webhookMessage.guild?.members?.fetch(registeredUser.discordId);
+
+        const { VERIFIED_ROLE_ID } = process.env;
+
+        // Give the user the verified role
+        // Issues I encountered:
+        // 1. The user is not in the guild
+        // 2. Missing Permissions: https://stackoverflow.com/q/62360928
+        await newMember!.roles.add(VERIFIED_ROLE_ID!);
+
+        webhookMessage.react('👌');
+        newMember!.send(
+          'Bienvenido al Discord de la FISI!!!\n\n'
+          + 'Has desbloqueado todo el servidor, gracias por registrarte.\n'
+          + '`#ProyectoDiscordDeLaFISI`',
+        ).catch(() => {});
+
+        // Save user to db
+        await collections.registrations?.insertOne(registeredUser);
+      }
+      catch (error) {
+        let errorMessage: string;
+
+        // Try to figure out what went wrong
+        if (error instanceof DiscordAPIError) {
+          errorMessage = newMember
+            ? `API error when registering: ${error}`
+            : `Can't fetch user: ${error}`;
+        }
+        else if (error instanceof MongoServerError) {
+          // Mongo fails, but we have fetched the user
+          errorMessage = `User registered, but could not be saved to DB: ${error.errmsg}`;
+          // react with warning
+          webhookMessage.react('⚠️');
+        }
+        else {
+          errorMessage = `Unknown error when registering: ${error}`;
+        }
+
+        // Send error feedback to the user
+        try {
+          newMember!.send(
+            'El equipo de FisiBot ha sufrido un problema (nuestro) al registrarte.\n\n'
+            + 'Estamos (_claramente_) solucionando el problema, pero mientras tanto, '
+            + 'puedes contactar a un administrador para que te registre manualmente.',
+          );
+          errorMessage += `. DM feedback webhookMessage sent to \`${newMember?.user.tag}\``;
+        }
+        catch (_error) {
+          errorMessage += `. Could not send DM to \`${registeredUser.discordId}\``;
+        }
+        // Send error feedback to the channel
+        webhookMessage.reply(errorMessage);
+        webhookMessage.react('❌');
+      }
+    }
+  },
+};
+
+export default MessageCreateHandler;
