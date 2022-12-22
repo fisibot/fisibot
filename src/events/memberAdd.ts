ad78b9b235d755b3f64df563faefc9ca82e999e9
leflores <pflores.fisi22@gmail.com>
2022-12-21 21:36:12 -0500

    feat: quarantine rol for unverified bots

--- src/events/memberAdd.ts
+++ src/events/memberAdd.ts
@@ -1,13 +1,25 @@
-import { Events, GuildMember } from 'discord.js';
+import { Events, GuildMember, UserFlags } from 'discord.js';
 import { FisiClientEventObject } from '@fisitypes';
 
 const memberAddHandler: FisiClientEventObject<Events.GuildMemberAdd> = {
   eventName: Events.GuildMemberAdd,
   handle: async (member: GuildMember) => {
     if (member.user.bot) {
-      const botRole = member.guild.roles.cache.find((role) => role.name === 'Bot');
-      if (botRole) await member.roles.add(botRole);
-      else console.log('Bot joined but no Bot role found.');
+      const isVerifiedBot = member.user.flags?.has(UserFlags.VerifiedBot);
+
+      if (isVerifiedBot) {
+        const botRole = member.guild.roles.cache.find((role) => role.name === 'Bot' || role.name === 'Bots');
+        if (botRole) await member.roles.add(botRole);
+        else console.log('Bot joined but no Bot role found.');
+      }
+      else {
+        const unverifiedBotRole = member.guild.roles.cache.find((role) => role.name === 'Cuarentena');
+        if (unverifiedBotRole) {
+          await member.roles.add(unverifiedBotRole);
+          console.log('⚠ ⚠ ⚠ Unverified bot joined and was put in quarantine.');
+        }
+        else console.log('Unverified bot joined but no Unverified Bot role found.');
+      }
     }
   },
 };
